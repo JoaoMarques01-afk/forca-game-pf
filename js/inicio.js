81232d4cd1ad35d8492bf49b46181e37d06023d7
Marcos Firmino <145605055+Marcos-Firmo@users.noreply.github.com>
2025-04-08 23:48:14 -0300

    Update inicio.js
    Adicionando comentários excluídos após upload de arquivos.
    
    obs.: os dois commit anteriores, são os códigos, o commit foi refeito para adicionar a descrição.

--- js/inicio.js
+++ js/inicio.js
@@ -1,20 +1,31 @@
+// Função que exibe ou oculta a área de créditos na tela inicial
 const botaoCreditos = () => {
     const fundo = document.querySelector(".fundo-preto")
     const creditos = document.querySelector(".creditos")
+    
+    // Exibe os créditos
     if(!fundo.classList.contains("ativado")){
         fundo.classList.add("ativado")
         creditos.classList.add("creditos-ativado")
+        
+    // Oculta os créditos
     } else {
         fundo.classList.remove("ativado")
         creditos.classList.remove("creditos-ativado")
     }
 }
+
+// Função que exibe ou oculta a área de tutorial na tela inicial
 const botaoTutorial = () => {
     const fundo = document.querySelector(".fundo-preto")
     const tutorial = document.querySelector(".tutorial")
+
+    // Exibe o tutorial
     if(!fundo.classList.contains("ativado")){
         fundo.classList.add("ativado")
         tutorial.classList.add("tutorial-ativado")
+
+    // Oculta o tutorial
     } else {
         fundo.classList.remove("ativado")
         tutorial.classList.remove("tutorial-ativado")
